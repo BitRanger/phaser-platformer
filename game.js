d8753da16c4684c9482f8de8cb7ca108b0882778
Nitro01010 <crgautam2020@gmail.com>
2022-05-15 16:15:57 -0700

    added variable jump & installed vite

--- game.js
+++ game.js
@@ -1,106 +1,147 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-		physics: {
-			default: 'arcade',
-			arcade: {
-				gravity: {y: 300},
-				debug: false
-			}
+	type: Phaser.AUTO,
+	width: 800,
+	height: 400,
+	fps: {
+		target: 144,
+		forceSetTimeOut: true,
+	},
+	transparent: true,
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { y: 300 },
+			debug: true,
 		},
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update,
+	},
 };
+
+var player,
+	coins,
+	platforms,
+	cursors,
+	score = 0,
+	scoreText,
+	acceleration = 750,
+	jumpVelocity = -300,
+	jumptimer = 0;
+var maxX;
 
 var game = new Phaser.Game(config);
 
-var platforms;
-
-function preload ()
-{
-  this.load.image('sky', 'assets/sky.png');
-	this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-	this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 
-    'assets/dude.png',
-    { frameWidth: 32, frameHeight: 48 }
-  );
+function preload() {
+	this.load.setBaseURL("https://digitherium.com/codepen/phaserplatformer/");
+	this.load.image("ground", "platform.jpg");
+	this.load.image("coin", "coin.jpg");
+	this.load.image("hero", "hero.jpg");
+	this.load.image("logo", "digitherium-logo.jpg");
 }
 
-
-function create ()
-{
-	this.add.image(400, 300, 'sky');
-
+function create() {
 	platforms = this.physics.add.staticGroup();
 
-	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(400, 400, "ground").setScale(2).refreshBody();
+	platforms.create(50, 240, "ground");
+	platforms.create(750, 140, "ground");
 
-	platforms.create(600, 400, 'ground');
-	platforms.create(50, 250, 'ground');
-	platforms.create(750, 220, 'ground');
+	player = this.physics.add.sprite(100, 350, "hero");
 
-	player = this.physics.add.sprite(100, 450, 'dude');
-
-	// player.setBounce(0.2);
 	player.setCollideWorldBounds(true);
-	player.body.setGravityY(300)
 
-	this.anims.create({
-		key: 'left',
-		frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-		frameRate: 10,
-		repeat: -1
-	});
-	
-	this.anims.create({
-		key: 'turn',
-		frames: [ {key: 'dude', frame: 4}],
-		frameRate: 20,
+	//Set bounce to 0, so our hero just lands directly
+	player.setBounce(0);
+
+	//Set top speeds
+	player.body.maxVelocity.x = 200;
+	player.body.maxVelocity.y = 500;
+
+	//Set gravity
+	player.body.setGravityY(300);
+	maxX = player.body.y;
+
+	cursors = this.input.keyboard.createCursorKeys();
+
+	coins = this.physics.add.group({
+		key: "coin",
+		repeat: 11,
+		setXY: { x: 12, y: 0, stepX: 70 },
 	});
 
-	this.anims.create({
-		key: 'right',
-		frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-		frameRate: 10,
-		repeat: -1
+	coins.children.iterate(function (child) {
+		child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+	});
+
+	scoreText = this.add.text(16, 16, "score: 0", {
+		fontSize: "24px",
+		fill: "#fff",
 	});
 
 	this.physics.add.collider(player, platforms);
-
-	arrows = this.input.keyboard.createCursorKeys();
+	this.physics.add.collider(coins, platforms);
+	this.physics.add.overlap(player, coins, collectCoin, null, this);
 }
 
-function update ()
-{
-	if (arrows.left.isDown)
-	{
-		player.setVelocityX(-160);
-
-		player.anims.play('left', true);
+function update() {
+	var standing = player.body.blocked.down || player.body.touching.down;
+	//if left key is down then move left
+	if (cursors.left.isDown) {
+		//if hero is on ground then use full acceleration
+		if (standing) {
+			player.setAccelerationX(-acceleration);
+		}
+		//if hero is in the air then accelerate slower
+		else {
+			player.setAccelerationX(-acceleration / 3);
+		}
+	}
+	//same deal but for right arrow
+	else if (cursors.right.isDown) {
+		if (standing) {
+			player.setAccelerationX(acceleration);
+		} else {
+			player.setAccelerationX(acceleration / 3);
+		}
+	}
+	//if neither left or right arrow is down then...
+	else {
+		//if hero is close to having no velocity either left or right then set velocity to 0. This stops jerky back and forth as the hero comes to a halt. i.e. as we slow hero down, below a certain point we just stop them moving altogether as it looks smoother
+		if (
+			Math.abs(player.body.velocity.x) < 10 &&
+			Math.abs(player.body.velocity.x) > -10
+		) {
+			player.setVelocityX(0);
+			player.setAccelerationX(0);
+		}
+		//if our hero isn't moving left or right then slow them down
+		else {
+			//this velocity.x check just works out whether we are setting a positive (going right) or negative (going left) number
+			player.setAccelerationX(
+				((player.body.velocity.x > 0 ? -1 : 1) * acceleration) / 2
+			);
+		}
 	}
 
-	else if (arrows.right.isDown)
-	{
-		player.setVelocityX(160);
-
-		player.anims.play('right', true);
+	//If player is touching floor and up key is pressed then jump
+	if (cursors.up.isDown) {
+		if (standing && jumptimer == 0) {
+			jumptimer = 1;
+			player.setVelocityY(jumpVelocity);
+		} else if (jumptimer > 0 && jumptimer < 31) {
+			jumptimer++;
+			player.setVelocityY(jumpVelocity - jumptimer * 2);
+		}
+	} else {
+		jumptimer = 0;
 	}
+}
 
-	else
-	{
-		player.setVelocityX(0);
-
-		player.anims.play('turn');
-	}
-
-	if(arrows.up.isDown && player.body.touching.down)
-	{
-		player.setVelocityY(-500);
-	}
+function collectCoin(player, coin) {
+	coin.disableBody(true, true);
+	score += 10;
+	scoreText.setText("Score: " + score);
 }
